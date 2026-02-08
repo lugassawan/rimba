60c2d2fbed7a99905ee03c514cc0915b42ae4b60
lugassawan <lugassawan@gmail.com>
2026-02-08 19:32:18 +0700

    docs: add descriptions to prefix flag help text

--- cmd/prefix.go
+++ cmd/prefix.go
@@ -5,27 +5,28 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// prefixFlag maps a CLI flag name to its resolver.PrefixType.
+// prefixFlag maps a CLI flag name to its resolver.PrefixType and description.
 type prefixFlag struct {
 	Flag string
 	Type resolver.PrefixType
+	Desc string
 }
 
 // prefixFlags lists the non-default prefix types available as boolean flags.
 // "feature" is the default and does not need a flag.
 var prefixFlags = []prefixFlag{
-	{"bugfix", resolver.PrefixBugfix},
-	{"hotfix", resolver.PrefixHotfix},
-	{"docs", resolver.PrefixDocs},
-	{"test", resolver.PrefixTest},
-	{"chore", resolver.PrefixChore},
+	{"bugfix", resolver.PrefixBugfix, "Fixing minor bugs that are part of the normal workflow"},
+	{"hotfix", resolver.PrefixHotfix, "Urgent fixes that need to be patched directly in production"},
+	{"docs", resolver.PrefixDocs, "Changes related to documentation"},
+	{"test", resolver.PrefixTest, "Experiments or new tests that might not be merged"},
+	{"chore", resolver.PrefixChore, "Non-code tasks like dependency updates"},
 }
 
 // addPrefixFlags registers all prefix boolean flags on cmd and marks them mutually exclusive.
 func addPrefixFlags(cmd *cobra.Command) {
 	names := make([]string, len(prefixFlags))
 	for i, pf := range prefixFlags {
-		cmd.Flags().Bool(pf.Flag, false, "Use "+pf.Flag+"/ branch prefix")
+		cmd.Flags().Bool(pf.Flag, false, pf.Desc)
 		names[i] = pf.Flag
 	}
 	cmd.MarkFlagsMutuallyExclusive(names...)
