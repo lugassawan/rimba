79043924265856712fe14a8e80912920aba074cb
lugassawan <lugassawan@gmail.com>
2026-02-09 16:43:08 +0700

    feat: add ascii art banner to help output

---
+++ cmd/banner.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/lugassawan/rimba/internal/termcolor"
+	"github.com/spf13/cobra"
+)
+
+func printBanner(cmd *cobra.Command) {
+	noColor, _ := cmd.Flags().GetBool("no-color")
+	p := termcolor.NewPainter(noColor)
+
+	lines := []string{
+		`     _       _`,
+		` _ _(_)_ __ | |__  __ _`,
+		"| '_| | '  \\| '_ \\/ _` |",
+		`|_| |_|_|_|_|_.__/\__,_|`,
+	}
+
+	versionStr := "v" + Version()
+
+	for i, line := range lines {
+		colored := p.Paint(line, termcolor.Green, termcolor.Bold)
+		if i == 1 {
+			fmt.Fprintf(cmd.OutOrStdout(), "%s          %s\n", colored, p.Paint(versionStr, termcolor.Gray))
+		} else {
+			fmt.Fprintln(cmd.OutOrStdout(), colored)
+		}
+	}
+
+	fmt.Fprintln(cmd.OutOrStdout())
+}
