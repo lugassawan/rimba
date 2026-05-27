a670cd08084506950358081e6ff9821f774dd03a
lugassawan <lugassawan@gmail.com>
2026-05-27 14:53:57 +0700

    [feat] git: add MergeInProgress and MergeAbort helpers

--- internal/git/merge.go
+++ internal/git/merge.go
@@ -10,3 +10,15 @@ func Merge(r Runner, dir, branch string, noFF bool) error {
 	_, err := r.RunInDir(dir, args...)
 	return err
 }
+
+// MergeInProgress reports whether a merge is in progress in dir (MERGE_HEAD exists).
+func MergeInProgress(r Runner, dir string) bool {
+	_, err := r.RunInDir(dir, "rev-parse", "--verify", "-q", "MERGE_HEAD")
+	return err == nil
+}
+
+// MergeAbort runs `git merge --abort` in dir.
+func MergeAbort(r Runner, dir string) error {
+	_, err := r.RunInDir(dir, "merge", "--abort")
+	return err
+}
